44f819f5be436744bddc211a710e14d0f4ccd23f
Nick Martin <nicholas.martin@tharsus.co.uk>
2023-08-10 00:26:12 +0100

    chartjs sort of working

---
+++ src/lib/charts/TestChart.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import { Chart } from 'chart.js/auto';
+
+  import { onMount } from 'svelte';
+
+  const DATA_COUNT = 5;
+  const NUMBER_CFG = {count: DATA_COUNT, min: 0, max: 100};
+
+  const data = {
+  labels: [
+    'Red',
+    'Blue',
+    'Yellow'
+  ],
+  datasets: [{
+    label: 'My First Dataset',
+    data: [300, 50, 100],
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 205, 86)'
+    ],
+    hoverOffset: 4
+  }]
+};
+
+  const config = {
+    type: 'doughnut',
+    data: data,
+    options: {
+      responsive: true
+    }
+  };
+
+  onMount(()  => {
+    const ctx = document.getElementById('chart') as HTMLCanvasElement;
+    new Chart(ctx, config);
+  });
+</script>
+<div>
+<canvas id="chart"></canvas>
+</div>
--- src/routes/analysis/[slug]/+page.svelte
+++ src/routes/analysis/[slug]/+page.svelte
@@ -1,8 +1,8 @@
-<script>
-	import DiversityProfile from '$lib/charts/diversityProfile.svelte';
+<script lang="ts">
+    import Chart from "$lib/charts/TestChart.svelte";
 
     /** @type {import('./$types').PageData} */
-    export let data;
+    // export let data;
 </script>
 
 <h1>Here is your analysed data</h1>
@@ -10,4 +10,4 @@
 <p>Bookmark this page, you can return here or share your analysis with this link</p>
 
 <h2>Diversity Profile</h2>
-<DiversityProfile {data} />
+<Chart />
